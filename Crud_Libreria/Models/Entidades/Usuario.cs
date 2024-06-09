1c812597fa1762ce771328198a580e97d6b7dff0
ClaudioArielNunez <claudioariel_n@hotmail.com>
2024-06-09 12:24:31 -0300

    Clases agregadas

---
+++ Crud_Libreria/Models/Entidades/Usuario.cs
@@ -0,0 +1,12 @@
+﻿namespace Crud_Libreria.Models.Entidades
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string NombreUsuario { get; set; }
+        public string URLFotoPerfil { get; set; }
+        public string Correo { get; set; } 
+        public string Clave { get; set; } 
+
+    }
+}
