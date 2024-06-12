c534b16c6d9b35fd9389a45ccae9ba05283ede47
ClaudioArielNunez <claudioariel_n@hotmail.com>
2024-06-12 20:31:28 -0300

    agregamos clase e implementamos interfaz Listas

--- Crud_Libreria/Services/IServicioLista.cs
+++ Crud_Libreria/Services/IServicioLista.cs
@@ -1,6 +1,11 @@
-﻿namespace Crud_Libreria.Services
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Crud_Libreria.Services
 {
     public interface IServicioLista
     {
+        //Cargamos desplegables
+        Task<IEnumerable<SelectListItem>> GetListaAutores();
+        Task<IEnumerable<SelectListItem>> GetListaCategorias();
     }
 }
---
+++ Crud_Libreria/Services/ServicioLista.cs
@@ -0,0 +1,47 @@
+﻿using Crud_Libreria.Datalayer;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Crud_Libreria.Services
+{
+    public class ServicioLista : IServicioLista
+    {
+        private readonly LibreriaContext _context;
+
+        public ServicioLista(LibreriaContext context)
+        {
+            this._context = context;
+        }
+        public async Task<IEnumerable<SelectListItem>> GetListaAutores()
+        {
+            List<SelectListItem> list = await _context.Autores.Select(x => new SelectListItem
+            {
+                Text = x.Nombre,
+                Value = x.Id.ToString()
+            }).OrderBy(x => x.Text).ToListAsync();
+                        
+            //insertamos en la primera posicion un nuevo elemento
+            list.Insert(0, new SelectListItem
+            {
+                Text = "[Seleccione un autor.....]",
+                Value = "0"
+            });
+
+            return list;
+        }
+
+        public async Task<IEnumerable<SelectListItem>> GetListaCategorias()
+        {
+            List<SelectListItem> list = await _context.Categorias.Select(x => new SelectListItem { Text = x.Nombre, Value = x.Id.ToString()}).OrderBy(x =>x.Text).ToListAsync();
+
+            list.Insert(0, new SelectListItem
+            {
+                Text = "[Seleccione una categoria.....]",
+                Value = "0"
+            });
+
+            return list;
+        }
+    }
+}
